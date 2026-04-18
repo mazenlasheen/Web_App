5c37e0740f3c84fa89efcf906dba6de404b1ac7f
Yusuf Arslan <a.online2006@gmail.com>
2026-04-18 19:19:42 +0800

    The Global connection string variable

--- WEB_APPLICATION/Models/UserDAL.cs
+++ WEB_APPLICATION/Models/UserDAL.cs
@@ -7,5 +7,9 @@ namespace WEB_APPLICATION.Models
 {
     public class UserDAL
     {
+        public bool registerUser(string username , string password )  
+        {
+            string connection_string = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=learningPlatformDataBase;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30"
+        }
     }
 }
