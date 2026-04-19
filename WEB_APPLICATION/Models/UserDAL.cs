f4770265775953f80ddf74767499e20660ab28b9
Yusuf Arslan <a.online2006@gmail.com>
2026-04-19 20:03:27 +0800

    basically imported the file into the UserDAL.cs file

--- WEB_APPLICATION/Models/UserDAL.cs
+++ WEB_APPLICATION/Models/UserDAL.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Configuration;
-using System.Data.SqlClient ; 
+using System.Data.SqlClient ;
+using BCrypt.Net; 
 
 namespace WEB_APPLICATION.Models
 {
