2f83ffa67580359762049f650aa0e2b75e665f6c
Yusuf Arslan <a.online2006@gmail.com>
2026-04-28 08:40:36 +0800

    Alhamdullah the controller classes were Created

---
+++ WEB_APPLICATION/Controllers/EnrollmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEB_APPLICATION.Controllers
+{
+    public class EnrollmentController : Controller
+    {
+        // GET: Enrollment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
